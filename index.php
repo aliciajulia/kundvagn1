2e6efd5d665e6b74059cccd3a411b3d9cdd45cc1
alibro916 <alibro916@E29658.edu.linkoping.se>
2014-09-25 14:29:19 +0200

    gjort klart varor

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
 
-session_start()
+session_start();
 
+$_SESSION["kundis"] = array();
+
+$_SESSION["kundis"]["cykel"] = array("prod"=>1, "pris"=>3999,  "besk"=>"hållbar", "farg"=>"brun");
+$_SESSION["kundis"]["kattunge"] = array("prod"=>2, "pris"=>100,  "besk"=>"kramgo", "farg"=>"svartprickig");
+$_SESSION["kundis"]["hylla"] = array("prod"=>3, "pris"=>499,  "besk"=>"exklusiv", "farg"=>"vit");
+$_SESSION["kundis"]["toffs"] = array("prod"=>4, "pris"=>2,  "besk"=>"elastisk meed en ros på", "farg"=>"rosafluffig");
+$_SESSION["kundis"]["keps"] = array("prod"=>5, "pris"=>79,  "besk"=>"Bamsekeps", "farg"=>"Bamse-blå");
+$_SESSION["kundis"]["dator"] = array("prod"=>6, "pris"=>5099,  "besk"=>"HP Laptop", "farg"=>"slimegrön");
+$_SESSION["kundis"]["toapapper"] = array("prod"=>7, "pris"=>100,  "besk"=>"Lambi, 10-pack", "farg"=>"vit");
 ?>
 
 <!DOCTYPE html>
