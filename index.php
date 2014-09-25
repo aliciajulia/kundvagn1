7c1407bc8e96f2bcee503a75d0ed120caafaa77e
alibro916 <alibro916@E29658.edu.linkoping.se>
2014-09-25 14:40:05 +0200

    gjort html

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
-
 session_start();
 
 $_SESSION["kundis"] = array();
-
-$_SESSION["kundis"]["cykel"] = array("prod"=>1, "pris"=>3999,  "besk"=>"hållbar", "farg"=>"brun");
-$_SESSION["kundis"]["kattunge"] = array("prod"=>2, "pris"=>100,  "besk"=>"kramgo", "farg"=>"svartprickig");
-$_SESSION["kundis"]["hylla"] = array("prod"=>3, "pris"=>499,  "besk"=>"exklusiv", "farg"=>"vit");
-$_SESSION["kundis"]["toffs"] = array("prod"=>4, "pris"=>2,  "besk"=>"elastisk meed en ros på", "farg"=>"rosafluffig");
-$_SESSION["kundis"]["keps"] = array("prod"=>5, "pris"=>79,  "besk"=>"Bamsekeps", "farg"=>"Bamse-blå");
-$_SESSION["kundis"]["dator"] = array("prod"=>6, "pris"=>5099,  "besk"=>"HP Laptop", "farg"=>"slimegrön");
-$_SESSION["kundis"]["toapapper"] = array("prod"=>7, "pris"=>100,  "besk"=>"Lambi, 10-pack", "farg"=>"vit");
+//varor
+$_SESSION["kundis"]["cykel"] = array("prod" => 1, "pris" => 3999, "besk" => "hållbar", "farg" => "brun", "antal" => 0);
+$_SESSION["kundis"]["kattunge"] = array("prod" => 2, "pris" => 100, "besk" => "kramgo", "farg" => "svartprickig", "antal" => 0);
+$_SESSION["kundis"]["hylla"] = array("prod" => 3, "pris" => 499, "besk" => "exklusiv", "farg" => "vit", "antal" => 0);
+$_SESSION["kundis"]["toffs"] = array("prod" => 4, "pris" => 2, "besk" => "elastisk meed en ros på", "farg" => "rosafluffig", "antal" => 0);
+$_SESSION["kundis"]["keps"] = array("prod" => 5, "pris" => 79, "besk" => "Bamsekeps", "farg" => "Bamse-blå", "antal" => 0);
+$_SESSION["kundis"]["dator"] = array("prod" => 6, "pris" => 5099, "besk" => "HP Laptop", "farg" => "slimegrön", "antal" => 0);
+$_SESSION["kundis"]["toapapper"] = array("prod" => 7, "pris" => 100, "besk" => "Lambi, 10-pack", "farg" => "vit", "antal" => 0);
 ?>
 
 <!DOCTYPE html>
@@ -20,8 +19,63 @@ $_SESSION["kundis"]["toapapper"] = array("prod"=>7, "pris"=>100,  "besk"=>"Lambi
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="varor">
+            <form>
+                Cykel
+                <input type="number" value="1"> 
+                <input type="submit" value="Lägg till"> 
+                <input type="hidden" value="cykel" name="prod1">
+            </form>
+        </div>
+        <div class="varor">
+            <form>
+                Kattunge
+                <input type="number" value="1"> 
+                <input type="submit" value="Lägg till"> 
+                <input type="hidden" value="katunge" name="prod1">
+            </form>
+        </div>
+        <div class="varor">
+            <form>
+                Hylla
+                <input type="number" value="1"> 
+                <input type="submit" value="Lägg till"> 
+                <input type="hidden" value="hylla" name="prod1">
+            </form>
+        </div>
+        <div class="varor">
+            <form>
+                Toffs
+                <input type="number" value="1"> 
+                <input type="submit" value="Lägg till"> 
+                <input type="hidden" value="toffs" name="prod1">
+            </form>
+        </div>
+        <div class="varor">
+            <form>
+                Keps
+                <input type="number" value="1"> 
+                <input type="submit" value="Lägg till"> 
+                <input type="hidden" value="keps" name="prod1">
+            </form>
+        </div>
+        <div class="varor">
+            <form>
+                Dator
+                <input type="number" value="1"> 
+                <input type="submit" value="Lägg till"> 
+                <input type="hidden" value="dator" name="prod1">
+            </form>
+        </div>
+        <div class="varor">
+            <form>
+                Toapapper
+                <input type="number" value="1"> 
+                <input type="submit" value="Lägg till"> 
+                <input type="hidden" value="toapapper" name="prod1">
+            </form>
+        </div>  
+
+
     </body>
 </html>
