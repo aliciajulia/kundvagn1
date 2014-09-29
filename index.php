8fbe3c4fe7b8b93b22e2bdd25d11e66d954a0af4
alibro916 <alibro916@E29603.edu.linkoping.se>
2014-09-29 10:55:17 +0200

    fel

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
 session_start();
 
-$_SESSION["kundis"] = array();
-//varor
-$_SESSION["kundis"]["cykel"] = array("prod" => 1, "pris" => 3999, "besk" => "hållbar", "farg" => "brun", "antal" => 0);
-$_SESSION["kundis"]["kattunge"] = array("prod" => 2, "pris" => 100, "besk" => "kramgo", "farg" => "svartprickig", "antal" => 0);
-$_SESSION["kundis"]["hylla"] = array("prod" => 3, "pris" => 499, "besk" => "exklusiv", "farg" => "vit", "antal" => 0);
-$_SESSION["kundis"]["toffs"] = array("prod" => 4, "pris" => 2, "besk" => "elastisk meed en ros på", "farg" => "rosafluffig", "antal" => 0);
-$_SESSION["kundis"]["keps"] = array("prod" => 5, "pris" => 79, "besk" => "Bamsekeps", "farg" => "Bamse-blå", "antal" => 0);
-$_SESSION["kundis"]["dator"] = array("prod" => 6, "pris" => 5099, "besk" => "HP Laptop", "farg" => "slimegrön", "antal" => 0);
-$_SESSION["kundis"]["toapapper"] = array("prod" => 7, "pris" => 100, "besk" => "Lambi, 10-pack", "farg" => "vit", "antal" => 0);
-?>
 
+//varor
+//$_SESSION["kundis"]["cykel"] = array("prod" => 1, "pris" => 3999, "besk" => "hållbar", "farg" => "brun", "antal" => 0);
+//$_SESSION["kundis"]["kattunge"] = array("prod" => 2, "pris" => 100, "besk" => "kramgo", "farg" => "svartprickig", "antal" => 0);
+//$_SESSION["kundis"]["hylla"] = array("prod" => 3, "pris" => 499, "besk" => "exklusiv", "farg" => "vit", "antal" => 0);
+//$_SESSION["kundis"]["toffs"] = array("prod" => 4, "pris" => 2, "besk" => "elastisk meed en ros på", "farg" => "rosafluffig", "antal" => 0);
+//$_SESSION["kundis"]["keps"] = array("prod" => 5, "pris" => 79, "besk" => "Bamsekeps", "farg" => "Bamse-blå", "antal" => 0);
+//$_SESSION["kundis"]["dator"] = array("prod" => 6, "pris" => 5099, "besk" => "HP Laptop", "farg" => "slimegrön", "antal" => 0);
+//$_SESSION["kundis"]["toapapper"] = array("prod" => 7, "pris" => 100, "besk" => "Lambi, 10-pack", "farg" => "vit", "antal" => 0);
+//
+
+if (!isset($_SESSION["kundis"])){
+    $_SESSION["kundis"] = array();
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Kundvagn</title>
     </head>
     <body>
         <div class="varor">
